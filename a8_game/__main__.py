de216f909230e27d8ea19de645091b0be8efbb0c
ishita2110agrawal <ishita2110agrawal@gmail.com>
2025-05-11 00:08:50 +0200

    board display done

--- a8_game/__main__.py
+++ a8_game/__main__.py
@@ -1,3 +1,26 @@
+import pygame
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Reversed Pacman')
+image = pygame.image.load('images/Board.png')
+image = pygame.transform.scale(image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+run = True
+while run:
+    screen.blit(image, (0, 0))
+    key = pygame.key.get_pressed()
+    """
+        if key[pygame.K_a] == True
+        player.move_ip(-1,0)
+    """
+
+    for event in pygame.event.get(): #event handler such as mouse clicks
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+pygame.quit()
+
 def main() -> None:
     # TODO: Your implementation here
     pass
