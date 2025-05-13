d7cacd78eda26f7f14c1f95a226db8f7a779cf2e
ishita2110agrawal <ishita2110agrawal@gmail.com>
2025-05-13 09:35:17 +0200

    board display done

--- a8_game/__main__.py
+++ a8_game/__main__.py
@@ -1,5 +1,7 @@
 import pygame
 import queue
+
+
 from map import grid
 
 # Constants
@@ -13,7 +15,7 @@ WHITE = (255, 255, 255)
 
 # Initialize game variables
 current_direction = None
-pacman_pos = [7, 7]
+pacman_pos = (7, 7)
 ghost_pos = [1, 1]
 ghost_positions = [(1, 1)]
 lives = 3
@@ -22,6 +24,7 @@ game_over = False
 
 # Get Pac-Man & Ghost positions
 def get_positions() -> None:
+
     global pacman_pos, ghost_pos, ghost_positions
     for row_idx, row in enumerate(grid):
         for col_idx, cell in enumerate(row):
@@ -29,7 +32,7 @@ def get_positions() -> None:
                 pacman_pos = [row_idx, col_idx]
             elif cell == 3:
                 ghost_pos = [row_idx, col_idx]
-                ghost_positions.append([row_idx, col_idx])
+                ghost_positions.append((row_idx, col_idx))
 
 def move_ghost(direction, screen) -> None:
     global ghost_pos
@@ -48,10 +51,21 @@ def move_ghost(direction, screen) -> None:
         grid[row][col] = 0  # Clear old position
         grid[new_row][new_col] = 3  # Move ghost
         ghost_pos = [new_row, new_col]
+        move_pacman()
 
     check_collision(screen)
 
 # The function for moving pacman
+def move_pacman() -> None:
+    global pacman_pos
+
+    print("Pacman moved")
+    print("Pacman position:", pacman_pos)
+    direction = bfs_alg()
+
+    print(pacman_pos)
+    pacman_pos = tuple_add(pacman_pos, direction)
+    print(pacman_pos)
 
 def check_collision(screen):
     global lives, pacman_pos, ghost_pos, ghost_positions, current_direction
@@ -93,6 +107,67 @@ def draw_maze(screen, ghost1, pacman) -> None:
             elif cell == 4:
                 screen.blit(pacman, (pacman_pos[1] * GRID_SIZE, pacman_pos[0] * GRID_SIZE))
 
+def tuple_add(t1: tuple[int, int], t2: tuple[int, int]) -> tuple[int, int]:
+    return t1[0] + t2[0], t1[1] + t2[1]
+def inverse_tuple(t1: tuple[int, int]) -> tuple[int, int]:
+    return -t1[0], -t1[1]
+
+
+def bfs_alg():
+    global ghost_pos, ghost_positions, pacman_pos
+    ghost_set = set()
+    for ghost in ghost_positions:
+        ghost_set.add(ghost)
+    visited = set()
+    visited.add(pacman_pos)
+    visit_grid = grid.copy()
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    q = queue.Queue()
+    q.put(pacman_pos)
+    counter = 3
+    visit_grid[pacman_pos[0]][pacman_pos[1]] = 2
+    temp_q = queue.Queue()
+    flag = True
+    nearest_ghost = None
+    while not q.empty() and flag:
+        print(counter)
+        while not q.empty() and flag:
+            pos = q.get()
+            for direction in directions:
+                next_pos = tuple_add(pos, direction)
+                if visit_grid[next_pos[0]][next_pos[1]] == 0:
+                    visit_grid[next_pos[0]][next_pos[1]] = counter
+                    temp_q.put(next_pos)
+                if next_pos in ghost_set:
+                    flag = False
+                    nearest_ghost = pos
+                    break
+
+        del q
+        q = temp_q
+        temp_q = queue.Queue()
+        counter += 1
+    counter -= 1
+    pacman_reached = False
+    last_direction = None
+    current_position = nearest_ghost
+    while not pacman_reached:
+        print(counter)
+        counter -= 1
+        for direction in directions:
+            print("direction", direction)
+            print(current_position)
+            new_pos = tuple_add(current_position, direction)
+            print(new_pos)
+            if visit_grid[new_pos[0]][new_pos[1]] == counter:
+                last_direction = direction
+                current_position = new_pos
+                print(current_position)
+                if pacman_pos == new_pos:
+                    pacman_reached = True
+                    return inverse_tuple(last_direction)
+                break
+
 def main() -> None:
     global current_direction
     pygame.init()
@@ -132,10 +207,18 @@ def main() -> None:
                     current_direction = "RIGHT"
 
         if current_direction:
+            try:
+                #move_pacman()
+                pass
+            except Exception as e:
+                run = False
+                pygame.quit()
+                raise e
             move_ghost(current_direction, screen)
         draw_maze(screen, ghost1, pacman)
         pygame.display.flip()
         clock.tick(2)
+
 
 if __name__ == "__main__":
     main()
